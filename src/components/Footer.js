2b82e3bbe33796ec8eff004672cb9e4feadd794f
Pjanarthana <janarthananoffl@gmail.com>
2024-10-01 15:06:13 +0530

    lasts commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
     <footer className="footer">
       <p>Made with ❤️ for You da Thambiii &#128540;</p>
       <div className="social-icons">
-        <a className="social-icon">📘</a>
-        <a className="social-icon">📸</a>
-        <a className="social-icon">🐦</a>
+        <a href="https://www.instagram.com/" className="social-icon">📘</a>
+        <a href="https://www.instagram.com/" className="social-icon">📸</a>
+        <a href="https://www.instagram.com/" className="social-icon">🐦</a>
       </div>
     </footer>
   );
